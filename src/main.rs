f6f2da23b4b5ed36ceb59faf4c33dc2df38eedfd
Taras Protsyshyn <protsyshyn.t@gmail.com>
2025-01-31 16:48:52 +0000

    CRUD for Epics and Stories

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+mod db;
+mod models;
+
+use dotenv::dotenv;
+
+fn main() {
+    dotenv().ok();
+
+    println!("Welcome To My-Jira!",);
+}
---
+++ src/models.rs
@@ -0,0 +1,53 @@
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
+pub enum Status {
+    Open,
+    InProgress,
+    Resolved,
+    Closed,
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
+pub struct Epic {
+    pub name: String,
+    pub description: String,
+    pub status: Status,
+    pub stories: Vec<u32>,
+}
+
+impl Epic {
+    pub fn new(name: String, description: String) -> Self {
+        Epic {
+            name,
+            description,
+            status: Status::Open,
+            stories: Vec::new(),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
+pub struct Story {
+    pub name: String,
+    pub description: String,
+    pub status: Status,
+}
+
+impl Story {
+    pub fn new(name: String, description: String) -> Self {
+        Story {
+            name,
+            description,
+            status: Status::Open,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
+pub struct DBState {
+    pub last_item_id: u32,
+    pub epics: HashMap<u32, Epic>,
+    pub stories: HashMap<u32, Story>,
+}
